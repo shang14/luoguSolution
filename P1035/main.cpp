f685d240f30ce387706197772365120664edaa1d
shang14 <shang14@gmail.com>
2022-11-13 00:33:17 +0800

    Create main.cpp

---
+++ P1035/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int k;
+    int i;
+    double j = 0.0;
+    cin >> k;
+
+    for (i = 1; ; ++i ){
+        j = j + 1.0/i;
+        if (j > k) {
+            break;
+        }
+
+
+    }
+    cout << i ;
+    return 0;
+
+
+}
